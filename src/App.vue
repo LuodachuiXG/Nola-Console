15c5007bad192af09d7d0a487f4d2f845cf34765
Loac <admin@loac.cc>
2024-02-09 03:36:05 +0800

    搭建基础框架

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,131 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { Message } from "@arco-design/web-vue";
+import {
+  IconCaretRight,
+  IconCaretLeft,
+  IconHome,
+  IconCalendar,
+} from "@arco-design/web-vue/es/icon";
+
+const collapsed = ref(false);
+const onCollapse = () => {
+  collapsed.value = !collapsed.value;
+  document.body.setAttribute("arco-theme", "dark");
+};
+
+const onClickMenuItem = (key: number) => {
+  Message.info({ content: `You select ${key}`, showIcon: true });
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <a-layout class="layout-demo">
+    <a-layout-sider hide-trigger collapsible :collapsed="collapsed">
+      <div class="logo" />
+      <a-menu
+        :defaultOpenKeys="['1']"
+        :defaultSelectedKeys="['0_3']"
+        :style="{ width: '100%' }"
+        @menuItemClick="onClickMenuItem"
+      >
+        <a-menu-item key="0_1" disabled>
+          <IconHome />
+          Menu 1
+        </a-menu-item>
+        <a-menu-item key="0_2">
+          <IconCalendar />
+          Menu 2
+        </a-menu-item>
+        <a-menu-item key="0_3">
+          <IconCalendar />
+          Menu 3
+        </a-menu-item>
+        <a-sub-menu key="1">
+          <template #title>
+            <span><IconCalendar />Navigation 1</span>
+          </template>
+          <a-menu-item key="1_1">Menu 1</a-menu-item>
+          <a-menu-item key="1_2">Menu 2</a-menu-item>
+          <a-sub-menu key="2" title="Navigation 2">
+            <a-menu-item key="2_1">Menu 1</a-menu-item>
+            <a-menu-item key="2_2">Menu 2</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="3" title="Navigation 3">
+            <a-menu-item key="3_1">Menu 1</a-menu-item>
+            <a-menu-item key="3_2">Menu 2</a-menu-item>
+            <a-menu-item key="3_3">Menu 3</a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+        <a-sub-menu key="4">
+          <template #title>
+            <span><IconCalendar />Navigation 4</span>
+          </template>
+          <a-menu-item key="4_1">Menu 1</a-menu-item>
+          <a-menu-item key="4_2">Menu 2</a-menu-item>
+          <a-menu-item key="4_3">Menu 3</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="padding-left: 20px">
+        <a-button shape="round" @click="onCollapse">
+          <IconCaretRight v-if="collapsed" />
+          <IconCaretLeft v-else />
+        </a-button>
+      </a-layout-header>
+      <a-layout>
+        <a-layout-content>123123123</a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.layout-demo {
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.layout-demo :deep(.arco-layout-sider) .logo {
+  height: 32px;
+  margin: 12px 8px;
+  background: rgba(255, 255, 255, 0.2);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.layout-demo :deep(.arco-layout-sider-light) .logo {
+  background: var(--color-fill-2);
+}
+
+.layout-demo :deep(.arco-layout-header) {
+  height: 64px;
+  line-height: 64px;
+  background: var(--color-bg-3);
+}
+
+.layout-demo :deep(.arco-layout-footer) {
+  height: 48px;
+  color: var(--color-text-2);
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 48px;
+  background: var(--color-bg-3);
+}
+
+.layout-demo :deep(.arco-layout-content) {
+  color: var(--color-text-2);
+  font-weight: 400;
+  font-size: 14px;
+  background: var(--color-bg-3);
+}
+
+.layout-demo :deep(.arco-layout-footer),
+.layout-demo :deep(.arco-layout-content) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 16px;
+  font-stretch: condensed;
+  text-align: center;
 }
 </style>
